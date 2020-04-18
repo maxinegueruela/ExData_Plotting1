7a945ccb760245b59785ab69a0c7d9627c5c998f
maxinegueruela <maxi.fer@live.com.ar>
2020-04-18 17:34:24 -0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -8,7 +8,7 @@ lines(house_data$house_time, house_data$Sub_metering_3, col = "blue")
 
 
 legend("topright", col=c("black", "red", "blue"), lty= c(1,1,1), lwd = c(0.5,0.5), 
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.5)
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.copy(png,"plot3.png", width=480, height=480)
 dev.off()
