e16c46f8dda80bc5c2339d26f5c89e0b3f2e7e97
maxinegueruela <maxi.fer@live.com.ar>
2020-04-18 17:31:51 -0300

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,15 @@
+source("load_data.R")
+
+#plot 3
+
+plot(house_data$house_time, house_data$Sub_metering_1, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+lines(house_data$house_time, house_data$Sub_metering_2, col = "red")
+lines(house_data$house_time, house_data$Sub_metering_3, col = "blue")
+
+
+legend("topright", col=c("black", "red", "blue"), lty= c(1,1,1), lwd = c(0.5,0.5), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.5)
+
+dev.copy(png,"plot3.png", width=480, height=480)
+dev.off()
+
